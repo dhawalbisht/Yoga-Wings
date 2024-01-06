26f8d94afe0a1b234ff5acab180678d2323394d6
Dhawal Bisht <dhawalbisht4543@gmail.com>
2024-01-06 21:04:02 +0530

    blogs

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import CardImage1 from '../assets/images/card-1.png'
-import CardImage1 from '../assets/images/card-2.png'
-import CardImage1 from '../assets/images/card-3.png'
 const Blogs = () => {
   return <div>Blogs</div>;
 };
