7b42f7da4e31ee668ca6a6a7e682304b120b4fd7
Dhawal Bisht <dhawalbisht4543@gmail.com>
2024-01-06 17:53:40 +0530

    updated

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
-
+import cardImage1 from '../assets/images/cards/card-1.png'
+import cardImage2 from '../assets/images/cards/card-2.png'
+import cardImage3 from '../assets/images/cards/card-3.png'
 const Cards = () => {
-  return <div>Cards</div>;
+  return <section>
+    <div classname="container mx-auto">
+      {/* { cards group  } */}
+      <div>
+        {/* { card 1} */}
+        <div>Card 1</div>
+      </div>
+      <div>
+        {/* { card 2} */}
+        <div>Card 2</div>
+      </div>
+      <div>
+        {/* { card 3} */}
+        <div>Card 3</div>
+      </div>
+
+    </div>
+  </section>;
 };
 
 export default Cards;
