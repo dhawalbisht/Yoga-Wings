241e46adcf637004174b1edb90269cafefe3b309
Dhawal Bisht <dhawalbisht4543@gmail.com>
2024-01-14 23:10:52 +0530

    conflicts resolved

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Blogs from './components/Blogs';
+import Cards from "./components/Cards";
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import Header from './components/Header';
@@ -10,35 +11,15 @@ import NavMobile from "./components/NavMobile";
 import NewsLetter from './components/NewsLetter';
 
 export default function App() {
-<<<<<<< HEAD
   return (
     <>
       <Hero />
       <Cards />
-
-
-      {/* <BrowserRouter>
-       <Routes>
-         <Route path="/blogs" element={<Blogs />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/footer" element={<Footer />} />
-         <Route path="/header" element={<Header />} />
-         <Route path="/hero" element={<Hero />} />
-         <Route path="/nav" element={<Nav />} />
-         <Route path="/navmobile" element={<NavMobile />} />
-         <Route path="/newsletter" element={<NewsLetter />} />
-       </Routes>
-     </BrowserRouter>  */}
+      <Blogs />
+      <Contact />
+      <NewsLetter />
+      <Footer />
     </>
-=======
-  return (<>
-    <Hero />
-    <Blogs />
-    <Contact />
-    <NewsLetter />
-    <Footer />
-  </>
->>>>>>> 7a244f346e94db27dd81b21fa032c708c9430266
   );
 }
 
